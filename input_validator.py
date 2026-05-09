7322a10dcffd04f91ab98d80cac1bc6930b1aa44
mikey6453 <tvq3bn@virginia.edu>
2026-05-09 08:04:27 -0400

    input validator

--- input_validator.py
+++ input_validator.py
@@ -13,3 +13,39 @@ RegistrationService: takes a list of validators in its constructor. Its register
 
 from abc import abstractmethod, ABC
 
+class Validator(ABC):
+    @abstractmethod
+    def validate(self, input: str) -> bool:
+        pass
+
+
+class EmailValidator(Validator):
+    def validate(self, input: str) -> bool:
+        return "@" in input
+
+
+class PasswordValidator(Validator):
+    def validate(self, input) -> bool:
+        return len(input) >= 8
+    
+
+class RegistrationService:
+    def __init__(self, validators: list[Validator]):
+        self._validators = validators
+
+    
+    def register(self, input: str) -> None:
+        for validator in self._validators:
+            if not validator.validate(input):
+                print("Registration failed")
+                return
+    
+
+if __name__ == "__main__":
+    email_reg = RegistrationService([EmailValidator()])
+    email_reg.register("a.bcd@gmail.com")
+
+
+    password_reg = RegistrationService([PasswordValidator()])
+    password_reg.register("Password1234")
+
