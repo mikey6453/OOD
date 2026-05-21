24b30480c97e71ec8a63665eaecc9a7bf9c55fb3
Michael <tvq3bn@virginia.edu>
2026-05-20 22:20:09 -0400

    design a parking lot

---
+++ case_studies/parking_lot.py
@@ -0,0 +1,10 @@
+"""
+Requirements:
+
+“Imagine you’re arriving at a busy parking lot, eager to park your car. At the entrance, 
+you’re issued a ticket. You then drive in, find a spot suited to your vehicle’s size, and park. 
+Later, when you prepare to leave, you present your ticket at the exit, the system calculates your fee, 
+and the spot is freed up for the next vehicle. Behind the scenes, the parking lot is assigning spots based 
+on vehicle size, recording entry and exit times, and updating availability for new arrivals. Now, let’s 
+design a parking lot system that handles all this.”
+"""
